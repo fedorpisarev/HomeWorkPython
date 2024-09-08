ee3cef5a387124470cff0d3666722f30daa2b6b1
twent <fpisarev@bk.ru>
2024-09-08 23:34:11 +0700

    08.09.2024

---
+++ Peremennyye.py
@@ -0,0 +1,5 @@
+name = 'python'
+total_tasks = '12'
+hours_spent = '1,5'
+average_execution_time = '0,125'
+print(name,','' ''всего задач', total_tasks, 'затрачено часов:',hours_spent,',''среднее время выполнения', average_execution_time, 'часа' )
