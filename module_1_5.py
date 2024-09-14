71203da44f3667e14dd9b7bb7ce53612ffa6eaf8
twent <fpisarev@bk.ru>
2024-09-14 23:35:58 +0700

    module_1_5.py

--- module_1_5.py
+++ module_1_5.py
@@ -1,1 +1,15 @@
+#immutable_var = 200, "fish", True
+#print(immutable_var)
+#immutable_var[1] = 'рыба'
+#print(immutable_var)
+immutable_var = 200, "fish", True
+print(immutable_var)
+print('Это кортеж, а выше была попытка внести изменения в кортеж, в отличии от списка он не изменяем, такие дела.')
+mutable_list = [200, "fish", True]
+print(mutable_list)
+mutable_list[1] = 'рыба'
+print(mutable_list)
+print('А это был СПИСОК, его можно изменять и у нас получилось! Вот они для наглядности: Кортеж и Список')
+print(immutable_var)
+print(mutable_list)
 
