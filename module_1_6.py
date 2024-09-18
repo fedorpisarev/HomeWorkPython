b11971023986095188a40612318381560f059282
twent <fpisarev@bk.ru>
2024-09-18 18:41:51 +0700

    module_1_6.py

---
+++ module_1_6.py
@@ -0,0 +1,16 @@
+my_dict = {'мама': 89968337505, 'отец': 89965542665, 'Алена час не дорого': 88005553535}
+print(my_dict)
+print(my_dict['отец'])
+my_dict.update({'жена': 89237553940 , 'Андрей колектор' : 89994505707})
+print(my_dict)
+del my_dict['Алена час не дорого']
+print(my_dict)
+my_set = {1, 2, 2, True, 1, 'xyz', 'zxc', 'zxc', 3, True}
+print(my_set)
+my_set.add(5)
+print(my_set)
+my_set.add('Kazakhstan')
+print(my_set)
+my_set.remove(2)
+print(my_set)
+
