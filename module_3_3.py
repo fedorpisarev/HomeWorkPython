b73b6bba1a8ae7d789f0f06898418efddc460b4a
twent <fpisarev@bk.ru>
2024-10-03 18:55:31 +0700

    module_3_3

---
+++ module_3_3.py
@@ -0,0 +1,10 @@
+def print_params(a = 1, b = 'строка', c = True):
+    print(a, b, c)
+#print_params(1, 'строка', True)
+#print_params(b = 25) # оно работает
+#print_params(c = [1,2,3]) # оно тоже
+#values_list = [9, 'stroka', True]
+#print_params(*values_list)
+#print_params(**values_dict)
+values_list_2 = [54.32, 'Строка' ]
+print_params(*values_list_2, 42) # и снова работает!!!
