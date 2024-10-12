98a0f32eb4c0755935201df822ffa08643e23186
twent <fpisarev@bk.ru>
2024-10-12 14:57:45 +0700

    module_4_1

---
+++ fake_math.py
@@ -0,0 +1,10 @@
+def divide(first, second):
+    if second == 0:
+        return 'ошибка'
+    else:
+        res = first / second
+        return res
+
+
+
+
---
+++ true_math.py
@@ -0,0 +1,11 @@
+from math import inf
+def divide(first, second):
+    if second == 0:
+        return inf
+    else:
+        res = first / second
+        return res
+
+
+
+
