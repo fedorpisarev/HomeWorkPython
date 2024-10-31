e65ceeb11eeccbcd87c30ecb0ed457613b29542b
twent <fpisarev@bk.ru>
2024-10-31 10:56:22 +0700

    module_4.2.py

--- module_4_2.py
+++ module_4_2.py
@@ -1,5 +1,6 @@
 def test_function():
     def inner_function():
         print('Я в области видимости функции test_function')
-    inner_function()
-test_function()
+    inner_function() # ничего не выводит
+#inner_function() # выдает ошибку, вследствие различия пространства имён
+test_function() # все работает
