700a1efedb48afbd81e708f0e762ccf9697bc148
twent <fpisarev@bk.ru>
2024-10-31 11:26:26 +0700

    module_5.2.py

--- module_5_2.py
+++ module_5_2.py
@@ -2,6 +2,12 @@ class House:
     def __init__(self, name, number_of_floors):
         self.name = name
         self.number_of_floors = number_of_floors
+
+    def __len__(self):
+        return self.number_of_floors
+
+    def __str__(self):
+        return f"Название: {self.name}, кол-во этажей: {self.number_of_floors}"
     def go_to(self, new_floor):
         if new_floor > self.number_of_floors or new_floor < 1:
             print("Такого этажа не существует")
@@ -13,7 +19,14 @@ class House:
 
 
 
-h1 = House('ЖК Горский', 18)
-h2 = House('Домик в деревне', 2)
-h1.go_to(5)
-h2.go_to(10)
+h1 = House('ЖК Эльбрус', 10)
+h2 = House('ЖК Акация', 20)
+
+# __str__
+print(h1)
+print(h2)
+
+# __len__
+print(len(h1))
+print(len(h2))
+
