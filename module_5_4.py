b65ecd1f71b5e6ab5509a3e454d7a2eae34bd73e
twent <fpisarev@bk.ru>
2024-11-06 00:03:07 +0700

    module_5_4

--- module_5_4.py
+++ module_5_4.py
@@ -1,7 +1,15 @@
 class House:
+    houses_history = []
     def __init__(self, name, number_of_floors):
         self.name = name
         self.number_of_floors = number_of_floors
+
+
+    def __new__(cls, *args, **kwargs): # ,*args, **kwargs
+        cls.houses_history.append(args[0])
+        print(*cls.houses_history)
+        return super().__new__(cls)
+
 
     def __len__(self):
         return self.number_of_floors
@@ -43,27 +51,17 @@ class House:
             self.number_of_floors += value
         return self
 
+    def __del__(self):
+        print(f'{self.name} снесён, но он останется в истории')
+
 
 h1 = House('ЖК Эльбрус', 10)
+print(House.houses_history)
 h2 = House('ЖК Акация', 20)
+print(House.houses_history)
+h3 = House('ЖК Матрёшки', 20)
+print(House.houses_history)
 
-print(h1)
-print(h2)
-
-print(h1 == h2) # __eq__
-
-h1 = h1 + 10 # __add__
-print(h1)
-print(h1 == h2)
-
-h1 += 10 # __iadd__
-print(h1)
-
-h2 = 10 + h2 # __radd__
-print(h2)
-
-print(h1 > h2) # __gt__
-print(h1 >= h2) # __ge__
-print(h1 < h2) # __lt__
-print(h1 <= h2) # __le__
-print(h1 != h2) # __ne__
+# Удаление объектов
+del h2
+del h3
