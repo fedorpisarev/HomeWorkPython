2fd603f855af2264c297eda96d392e046c2cc377
twent <fpisarev@bk.ru>
2024-11-15 00:39:37 +0700

    mmodule_6_2.1

--- module_6_2.py).py
+++ module_6_2.py).py
@@ -3,27 +3,29 @@ class Vehicle:
     def __init__(self, owner, __model, __color, __engine_power):
         self.__COLOR_VARIANTS = list(map(str.lower, self.__COLOR_VARIANTS))
         self.owner = owner
-        self.__model = __model
-        self.__engine_power = __engine_power
-        self.__color = __color
+        self._model = __model
+        self.power = __engine_power
+        self._color = __color
     def get_model(self, model):
-        self.__model = model
+        pass
 
     def get_horsepower(self, power):
-        self.__engine_power = power
-        print(f"Мощность двигателя: {power}")
-    def get_color(self, __color):
-        self.__color = __color
-        print(f"Цвет: {__color}")
+        pass
+
+    def get_color(self, _color):
+        pass
+
+
     def print_info(self):
-        print(f"Модель: {self.__model}")
-        print(f"Мощность двигателя: {self.__engine_power}")
-        print(f"Цвет: {self.__color}")
+        print(f"Модель: {self._model}")
+        print(f"Мощность двигателя: {self.power}")
+        print(f"Цвет: {self._color}")
         print(f"Владелец: {self.owner}")
+
     def set_color(self, new_color):
         reg_colours = [colour.lower() for colour in self.__COLOR_VARIANTS]  # для проверки без учета регистра
         if new_color.lower() in reg_colours:
-            self.__color = new_color
+            self._color = new_color
         else:
             print(f'Нельзя сменить цвет на {new_color}')
 
@@ -31,12 +33,12 @@ class Vehicle:
 
 class Sedan(Vehicle):
     __PASSENGERS_LIMIT = 5
-    ...
 
 
 
 
-# Текущие цвета __COLOR_VARIANTS = ['blue', 'red', 'green', 'black', 'white']
+# Текущие цвета
+__COLOR_VARIANTS = ['blue', 'red', 'green', 'black', 'white']
 vehicle1 = Sedan('Fedos', 'Toyota Mark II', 'blue', 500)
 
 # Изначальные свойства
