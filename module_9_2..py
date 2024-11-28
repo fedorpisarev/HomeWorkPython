1fd2fa175ff55c0deefb9e4449d1c1408de152d3
twent <fpisarev@bk.ru>
2024-11-28 15:19:15 +0700

    module_9.2?2

--- module_9_2..py
+++ module_9_2..py
@@ -2,7 +2,7 @@
 
 first_strings = ['Elon', 'Musk', 'Programmer', 'Monitors', 'Variable']
 second_strings = ['Task', 'Git', 'Comprehension', 'Java', 'Computer', 'Assembler']
-zp = zip(first_strings, second_strings)
+
 
 first_result = [len(i) for i in first_strings if len(i) > 5]
 second_result = [(i, x) for i in first_strings for x in second_strings if len(i) == len(x)]
