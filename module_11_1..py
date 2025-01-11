68a560479962ccaf008e3eb73fc022a1fde61684
twent <fpisarev@bk.ru>
2025-01-11 11:12:26 +0700

    module_11_1.1py

--- module_11_1..py
+++ module_11_1..py
@@ -1,3 +1,7 @@
-import requests # делаем запрос на чтение страницы https://sky.pro/media/
-response = requests.get('http://www.constitution.ru/symbols/anthem.htm')
-print(response.text)
+import requests # делаем запрос на чтение страницы https://www.youtube.com/results
+url = 'https://www.youtube.com/results'
+query = {'search_query':'куплинов'}
+response = requests.get(url, params=query)
+
+print(response.url, 'url')
+print(response.text, 'text')
